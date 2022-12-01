2499880b10bb2647d6b3de26a6cd5a5ea75f7553
Julian Doherty <madlep@madlep.com>
2022-12-01 19:29:28 +1300

    day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,7 +1,63 @@
-pub fn part1(_data: String) -> String {
-    "TODO part 1".to_string()
+type Calories = u32;
+type ElfSupplies = Vec<Calories>;
+
+pub fn part1(data: String) -> String {
+    parse(data)
+        .iter()
+        .map(&total_elf_calories)
+        .max()
+        .unwrap()
+        .to_string()
 }
 
-pub fn part2(_data: String) -> String {
-    "TODO part 2".to_string()
+pub fn part2(data: String) -> String {
+    let mut elf_cals = parse(data)
+        .iter()
+        .map(&total_elf_calories)
+        .collect::<Vec<Calories>>();
+
+    elf_cals.sort();
+
+    elf_cals.iter().rev().take(3).sum::<Calories>().to_string()
+}
+
+fn parse(data: String) -> Vec<ElfSupplies> {
+    let mut elves: Vec<ElfSupplies>;
+    let last_elf: ElfSupplies;
+
+    (elves, last_elf) = data
+        .split("\n")
+        .map(parse_calories)
+        .fold((Vec::new(), Vec::new()), accumulate_calories);
+    if !last_elf.is_empty() {
+        elves.push(last_elf);
+    }
+
+    elves
+}
+
+fn accumulate_calories(
+    state: (Vec<ElfSupplies>, ElfSupplies),
+    calories: Option<Calories>,
+) -> (Vec<ElfSupplies>, ElfSupplies) {
+    let (mut all_elves, mut current_elf) = state;
+
+    match calories {
+        Some(cal) => {
+            current_elf.push(cal);
+        }
+        None => {
+            all_elves.push(current_elf);
+            current_elf = Vec::new()
+        }
+    };
+    (all_elves, current_elf)
+}
+
+fn parse_calories(calories: &str) -> Option<Calories> {
+    calories.parse::<Calories>().ok()
+}
+
+fn total_elf_calories(supplies: &ElfSupplies) -> Calories {
+    supplies.iter().sum()
 }
