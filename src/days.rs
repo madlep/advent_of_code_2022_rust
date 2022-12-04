f2fb1e270fc47e1326c6833f7938bcf6ee3c79a1
Julian Doherty <madlep@madlep.com>
2022-12-05 10:37:20 +1300

    day 4

--- src/days.rs
+++ src/days.rs
@@ -1,6 +1,7 @@
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 pub type Day = u8;
 pub type Part = u8;
@@ -13,6 +14,8 @@ pub fn run_day_part(day: Day, part: Part, data: String) -> String {
         (2, 2) => day2::part2(data),
         (3, 1) => day3::part1(data),
         (3, 2) => day3::part2(data),
+        (4, 1) => day4::part1(data),
+        (4, 2) => day4::part2(data),
         (day_m, part_m) => panic!("Day {}, part {} is not implemented", day_m, part_m),
     }
 }
---
+++ src/days/day4.rs
@@ -0,0 +1,88 @@
+pub fn part1(data: String) -> String {
+    data.lines()
+        .map(parse_line)
+        .filter(is_full_overlap)
+        .count()
+        .to_string()
+}
+
+pub fn part2(data: String) -> String {
+    data.lines()
+        .map(parse_line)
+        .filter(is_partial_overlap)
+        .count()
+        .to_string()
+}
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use std::ops::RangeInclusive;
+
+type Section = u32;
+
+fn parse_line(line: &str) -> (RangeInclusive<Section>, RangeInclusive<Section>) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+    }
+    let caps = RE.captures(line).unwrap();
+    let s1_start = &caps[1].parse::<Section>().unwrap();
+    let s1_end = &caps[2].parse::<Section>().unwrap();
+    let s2_start = &caps[3].parse::<Section>().unwrap();
+    let s2_end = &caps[4].parse::<Section>().unwrap();
+
+    let s1 = *s1_start..=*s1_end;
+    let s2 = *s2_start..=*s2_end;
+
+    (s1, s2)
+}
+
+fn is_full_overlap(ranges: &(RangeInclusive<Section>, RangeInclusive<Section>)) -> bool {
+    let (r1, r2) = ranges;
+    let (r1_s, r1_e) = r1.clone().into_inner();
+    let (r2_s, r2_e) = r2.clone().into_inner();
+
+    (r1_s <= r2_s && r1_e >= r2_e) || (r2_s <= r1_s && r2_e >= r1_e)
+}
+
+fn is_partial_overlap(ranges: &(RangeInclusive<Section>, RangeInclusive<Section>)) -> bool {
+    let (r1, r2) = ranges;
+    let (r1_s, r1_e) = r1.clone().into_inner();
+    let (r2_s, r2_e) = r2.clone().into_inner();
+
+    r1.contains(&r2_s) || r1.contains(&r2_e) || r2.contains(&r1_s) || r2.contains(&r1_e)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_calcultes_part1_given_example() {
+        let data = "2-4,6-8\n\
+                    2-3,4-5\n\
+                    5-7,7-9\n\
+                    2-8,3-7\n\
+                    6-6,4-6\n\
+                    2-6,4-8";
+        let result = part1(data.to_string());
+        assert_eq!(result, "2");
+    }
+
+    #[test]
+    fn it_calcultes_part2_given_example() {
+        let data = "2-4,6-8\n\
+                    2-3,4-5\n\
+                    5-7,7-9\n\
+                    2-8,3-7\n\
+                    6-6,4-6\n\
+                    2-6,4-8";
+        let result = part2(data.to_string());
+        assert_eq!(result, "4");
+    }
+
+    #[test]
+    fn it_parses_input_line() {
+        assert_eq!(parse_line(&"2-4,6-8"), (2..=4, 6..=8))
+    }
+}
