2ca9e352fd1e69b89278c63c2724127690da4438
Julian Doherty <madlep@madlep.com>
2022-12-05 18:08:28 +1300

    zero prefix day mods so they stay ordered when we get > 10

--- src/days.rs
+++ src/days.rs
@@ -1,21 +1,21 @@
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
 
 pub type Day = u8;
 pub type Part = u8;
 
 pub fn run_day_part(day: Day, part: Part, data: String) -> String {
     match (day, part) {
-        (1, 1) => day1::part1(data),
-        (1, 2) => day1::part2(data),
-        (2, 1) => day2::part1(data),
-        (2, 2) => day2::part2(data),
-        (3, 1) => day3::part1(data),
-        (3, 2) => day3::part2(data),
-        (4, 1) => day4::part1(data),
-        (4, 2) => day4::part2(data),
+        (1, 1) => day01::part1(data),
+        (1, 2) => day01::part2(data),
+        (2, 1) => day02::part1(data),
+        (2, 2) => day02::part2(data),
+        (3, 1) => day03::part1(data),
+        (3, 2) => day03::part2(data),
+        (4, 1) => day04::part1(data),
+        (4, 2) => day04::part2(data),
         (day_m, part_m) => panic!("Day {}, part {} is not implemented", day_m, part_m),
     }
 }
