8341c087cf631ed13766189b1e1ef286a15d3f02
Julian Doherty <madlep@madlep.com>
2022-12-07 21:53:41 +1300

    clean up parsed data structure

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,14 +1,6 @@
-use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{alphanumeric1, not_line_ending, u32},
-    combinator::map,
-    multi::{many0, separated_list0},
-    sequence::{preceded, separated_pair, terminated},
-    IResult,
-};
-
-pub fn part1(_data: String) -> String {
+pub fn part1(data: String) -> String {
+    let cmds = cmds_parser(&data);
+    dbg!(cmds.unwrap());
     "foobar".to_string()
 }
 
@@ -63,15 +55,32 @@ impl<'a> DirNode for File<'a> {
 
 #[derive(Debug, PartialEq)]
 enum Cmd {
-    Cd(String),
+    Cd(CdPath),
     Ls(Vec<LsOutput>),
 }
 
 #[derive(Debug, PartialEq)]
-enum LsOutput {
-    Dir(String),
-    File(u32, String),
+enum CdPath {
+    Root,
+    Parent,
+    Relative(String),
 }
+
+#[derive(Debug, PartialEq)]
+enum LsOutput {
+    DirOutput(String),
+    FileOutput(String, u32),
+}
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{not_line_ending, u32},
+    combinator::map,
+    multi::separated_list0,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
 
 fn cmds_parser(input: &str) -> IResult<&str, Vec<Cmd>> {
     let mut parser = separated_list0(tag("\n"), cmd_parser);
@@ -83,9 +92,20 @@ fn cmd_parser(input: &str) -> IResult<&str, Cmd> {
     parser(input)
 }
 
+fn cd_path_parser(input: &str) -> IResult<&str, CdPath> {
+    let root_parser = map(tag("/"), |_| CdPath::Root);
+    let parent_parser = map(tag(".."), |_| CdPath::Parent);
+    let relative_parser = map(not_line_ending, |name: &str| {
+        CdPath::Relative(name.to_string())
+    });
+
+    let mut parser = alt((root_parser, parent_parser, relative_parser));
+    parser(input)
+}
+
 fn cd_parser(input: &str) -> IResult<&str, Cmd> {
-    let parser = preceded(tag("cd "), alt((tag(".."), tag("/"), alphanumeric1)));
-    let mut cmd_parser = map(parser, |path: &str| Cmd::Cd(path.to_string()));
+    let parser = preceded(tag("cd "), cd_path_parser);
+    let mut cmd_parser = map(parser, |path| Cmd::Cd(path));
 
     cmd_parser(input)
 }
@@ -103,7 +123,7 @@ fn ls_parser(input: &str) -> IResult<&str, Cmd> {
 
 fn dir_parser(input: &str) -> IResult<&str, LsOutput> {
     let parser = preceded(tag("dir "), not_line_ending);
-    let mut ls_output_parser = map(parser, |name: &str| LsOutput::Dir(name.to_string()));
+    let mut ls_output_parser = map(parser, |name: &str| LsOutput::DirOutput(name.to_string()));
 
     ls_output_parser(input)
 }
@@ -111,7 +131,7 @@ fn dir_parser(input: &str) -> IResult<&str, LsOutput> {
 fn file_parser(input: &str) -> IResult<&str, LsOutput> {
     let parser = separated_pair(u32, tag(" "), not_line_ending);
     let mut ls_output_parser = map(parser, |(size, name): (u32, &str)| {
-        LsOutput::File(size, name.to_string())
+        LsOutput::FileOutput(name.to_string(), size)
     });
 
     ls_output_parser(input)
@@ -119,6 +139,7 @@ fn file_parser(input: &str) -> IResult<&str, LsOutput> {
 
 #[cfg(test)]
 mod tests {
+    use super::CdPath::*;
     use super::Cmd::*;
     use super::LsOutput::*;
     use super::*;
@@ -126,21 +147,29 @@ mod tests {
     #[test]
     fn it_parses_cd_command() {
         let (rest, cmd) = cd_parser("cd /\nnot cd stuff").unwrap();
-        assert_eq!(cmd, Cd("/".to_string()));
+        assert_eq!(cmd, Cd(Root));
+        assert_eq!(rest, "\nnot cd stuff");
+
+        let (rest, cmd) = cd_parser("cd ..\nnot cd stuff").unwrap();
+        assert_eq!(cmd, Cd(Parent));
+        assert_eq!(rest, "\nnot cd stuff");
+
+        let (rest, cmd) = cd_parser("cd foobar\nnot cd stuff").unwrap();
+        assert_eq!(cmd, Cd(Relative("foobar".to_string())));
         assert_eq!(rest, "\nnot cd stuff");
     }
 
     #[test]
     fn it_parses_dir_output() {
         let (rest, ls_output_line) = dir_parser("dir foobar\nnot dir stuff").unwrap();
-        assert_eq!(ls_output_line, Dir("foobar".to_string()));
+        assert_eq!(ls_output_line, DirOutput("foobar".to_string()));
         assert_eq!(rest, "\nnot dir stuff");
     }
 
     #[test]
     fn it_parses_file_output() {
         let (rest, ls_output_line) = file_parser("12345 foobar.baz\nnot file stuff").unwrap();
-        assert_eq!(ls_output_line, File(12345, "foobar.baz".to_string()));
+        assert_eq!(ls_output_line, FileOutput("foobar.baz".to_string(), 12345));
         assert_eq!(rest, "\nnot file stuff");
     }
 
@@ -157,10 +186,10 @@ not ls stuff";
         assert_eq!(
             ls,
             Ls(vec![
-                Dir("a".to_string()),
-                File(14848514, "b.txt".to_string()),
-                File(8504156, "c.dat".to_string()),
-                Dir("d".to_string())
+                DirOutput("a".to_string()),
+                FileOutput("b.txt".to_string(), 14848514),
+                FileOutput("c.dat".to_string(), 8504156),
+                DirOutput("d".to_string())
             ])
         );
         assert_eq!(rest, "\nnot ls stuff");
@@ -170,7 +199,7 @@ not ls stuff";
     fn it_parses_cd_or_ls_command() {
         let cd_input = "$ cd foo\n$ ls\n dir baz\n1234 file.txt\nnot command stuff";
         let (rest, command) = cmd_parser(cd_input).unwrap();
-        assert_eq!(command, Cd("foo".to_string()));
+        assert_eq!(command, Cd(Relative("foo".to_string())));
         assert_eq!(
             rest,
             "\n$ ls\n dir baz\n1234 file.txt\nnot command stuff".to_string()
@@ -207,30 +236,30 @@ $ ls
         assert_eq!(
             commands,
             vec![
-                Cd("/".to_string()),
+                Cd(Root),
                 Ls(vec![
-                    Dir("a".to_string()),
-                    File(14848514, "b.txt".to_string()),
-                    File(8504156, "c.dat".to_string()),
-                    Dir("d".to_string())
+                    DirOutput("a".to_string()),
+                    FileOutput("b.txt".to_string(), 14848514),
+                    FileOutput("c.dat".to_string(), 8504156),
+                    DirOutput("d".to_string())
                 ]),
-                Cd("a".to_string()),
+                Cd(Relative("a".to_string())),
                 Ls(vec![
-                    Dir("e".to_string()),
-                    File(29116, "f".to_string()),
-                    File(2557, "g".to_string()),
-                    File(62596, "h.lst".to_string()),
+                    DirOutput("e".to_string()),
+                    FileOutput("f".to_string(), 29116),
+                    FileOutput("g".to_string(), 2557),
+                    FileOutput("h.lst".to_string(), 62596),
                 ]),
-                Cd("e".to_string()),
-                Ls(vec![File(584, "i".to_string()),]),
-                Cd("..".to_string()),
-                Cd("..".to_string()),
-                Cd("d".to_string()),
+                Cd(Relative("e".to_string())),
+                Ls(vec![FileOutput("i".to_string(), 584)]),
+                Cd(Parent),
+                Cd(Parent),
+                Cd(Relative("d".to_string())),
                 Ls(vec![
-                    File(4060174, "j".to_string()),
-                    File(8033020, "d.log".to_string()),
-                    File(5626152, "d.ext".to_string()),
-                    File(7214296, "k".to_string()),
+                    FileOutput("j".to_string(), 4060174),
+                    FileOutput("d.log".to_string(), 8033020),
+                    FileOutput("d.ext".to_string(), 5626152),
+                    FileOutput("k".to_string(), 7214296),
                 ])
             ]
         );
